5ee391af99a142d26506df7aa3618e0411d5d30b
Miguel Navarro <miguelsn243@gmail.com>
2023-05-10 19:59:12 -0700

    Update demo
    Make it more stylish, add new graph per date

--- demo.py
+++ demo.py
@@ -7,9 +7,9 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 from getdatafromcsv import getData
+from functions import createEmptyFig
 stylesheet = ["https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"]
 app = Dash(__name__, external_stylesheets=stylesheet)
-
 colors = {
     'background': '#2D2727',
     'text': '#ffffff'
@@ -19,10 +19,9 @@ colors = {
 # see https://plotly.com/python/px-arguments/ for more options
 data, weeks = getData()
 print(weeks)
-print(data["Topic1"])
+print(data)
 
 fig = px.line(x=weeks, y=data["Topic1"], markers=True)
-
 fig.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
@@ -31,21 +30,43 @@ fig.update_layout(
     font_color="#ffffff"
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background'], "width": "100vw", "height": "100vh", "margin-top": "0"}, children=[
-    html.Nav(
-        children=[
-    html.H4(children='LDA Mañaneras',
+fig2 = createEmptyFig("Seleccione una semana", colors['background'])
+
+
+amlo = "assets/amlo.jpg"
+app.layout = html.Div(style={'backgroundColor': colors['background'], "margin-top": "0"}, children=[
+    html.H1(children = ["Topic Modeling de las conferencias diarias del presidente Andres Manuel López Obrador"],
             style={
-                "position" : "relative",
-                "left": "45%"
-            })
-        ],
-        style={
-            'textAlign': 'center',
-            'color': colors['text'],
-        },
-        className = "navbar navbar-dark bg-dark p-3 text-light"
-    ),
+            'color': colors['text']
+        },className = "text-center mt-5 mb-5"),
+    html.H5(children = "Rodrigo Nevarez Escobar, Kevin David Ruiz González, Miguel Sebastian Navarro Islas, Jesús Giovanni Corral Valdez",
+            className = "text-center mt-5 mb-2",
+            style={
+            'color': colors['text']
+        }),
+    #html.Div(children = [html.Div(children = [html.Img(src = amlo, style={'height':'50%', 'width':'50%'})], className = "row justify-content-center")], className = "container-fluid justify-content-center"),
+    html.H3(children = ["Introducción"],
+            style = {"color": colors["text"]},
+            className = "ml-3"),
+    html.P("""El modelado de temas es una tecnica que permite encontrar patrones y estructuras en amplias colecciones de texto. Una
+        de las tecnicas mas utilizadas para esto es el LDA (Latent Dirichlet Allocation) el cual es un modelo probabilistico que encuentra
+        temas en un texto basado en la frecuencia de las palabras. En este proyecto, se utiliza una implementación de LDA a un corpus 
+        formado por los títulos y primer párrafo de los boletines de las conferencias mañaneras del presidente. De esta forma, obtenemos una aproximación
+        de los temas más frecuentes e importantes en cierto periodo de tiempo.""",
+           style={"color": colors["text"]},
+           className = "ml-3"),
+    html.H3(children = ["Metodología"],
+            style = {"color": colors["text"]},
+            className = "ml-3"),
+    html.P("""pendiente jijijijiji""",
+           style={"color": colors["text"]},
+           className = "ml-3"),
+    html.H3(children = ["Visualización de los resultados"],
+            style = {"color": colors["text"]},
+            className = "ml-3"),
+    html.H4(children = ["Graficación por tema"],
+            style = {"color": colors["text"]},
+            className = "ml-3 mt-3"),
     html.Div(
     className = "p-3 row w-100",
     style = {
@@ -55,9 +76,10 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], "width": "
         html.Div(
             className = "col-sm",
             children = [
+            html.P("Temas:", style={"color": colors["text"]}),
             dcc.Input(
                 id="input",
-                className = "form-control col-sm w-50",
+                className = "form-control w-50",
                 type="text",
                 placeholder="Topic 1",
                 debounce = True,
@@ -71,7 +93,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], "width": "
             children = [
             dcc.Input(
                 id="input2",
-                className = "form-control col-sm float-right w-50",
+                className = "form-control col-sm float-right mt-5 w-50",
                 type="text",
                 placeholder="Topic 1",
                 debounce = True,
@@ -81,15 +103,28 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], "width": "
             ]
         )]
     ),
-        html.H4(children='Presencia del tema:',
-            className = "m-3",
-            style={
-            'color': colors['text']
-        },),
+      #  html.H4(children='Presencia del tema:',
+      #      className = "m-3",
+      #      style={
+      #      'color': colors['text']
+       # },),
     dcc.Graph(
         id='graph',
         figure=fig
-    )
+    ),
+    html.H4(children = ["Graficación por fecha"],
+        style = {"color": colors["text"]},
+        className = "ml-3 mt-3 mb-4"),
+    dcc.Dropdown(
+        weeks,
+        id = "drop",
+        placeholder="Selecciona una semana",
+        className = "w-25 ml-3"
+),
+dcc.Graph(
+        id='graph2',
+        figure=fig2
+    ),
 ])
 @app.callback(
     Output('graph', 'figure'),
@@ -107,37 +142,14 @@ def update_graph(topic, topic2):
         else:
             new_df = {topic2: new_df2}
             topics = [topic2]
-    else:
+    elif new_df1 is not None:
         new_df = {topic: new_df1}
         topics = [topic]
-    if new_df is None:
-        fig = go.Figure()
-        fig.update_layout(
-          transition_duration=500,
-          xaxis = {
-            "visible": False,
-          },
-          yaxis = {
-            "visible": False
-          },
-          plot_bgcolor=colors['background'],
-          paper_bgcolor=colors['background'],
-        )
-        fig.add_annotation(
-            text = "Tema no encontrado",
-            xref = "paper",
-            yref = "paper",
-            font=dict(
-                color="#ffffff",
-                size = 28
-            ),
-            showarrow = False,
-        )
-        return fig
+    if topics==[]:
+        return createEmptyFig("Tema no encontrado", colors['background'])
     fig = go.Figure()
     for topic in topics:
         fig.add_trace(go.Scatter(x=weeks, y=new_df[topic], name=topic))
-    #fig = px.line(x=weeks, y=new_df, markers=True)
     fig.update_layout(
         transition_duration=500,
         plot_bgcolor=colors['background'],
@@ -147,5 +159,29 @@ def update_graph(topic, topic2):
         font_color="#ffffff"
     )
     return fig
+
+@app.callback(
+    Output(component_id = "graph2", component_property="figure"),
+    Input(component_id = "drop", component_property="value"))
+def update_graph2(week):
+    if week not in weeks:
+        return createEmptyFig("Seleccione un tema", colors['background'])
+    index = weeks.index(week)
+    probs = {"Tema": [], "Probabilidad": []}
+    for x in data.keys():
+        probs["Tema"].append(x)
+        probs["Probabilidad"].append(data[x][index])
+    fig = px.bar(probs, x="Tema", y="Probabilidad")
+    fig.update_layout(
+        transition_duration=500,
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        xaxis_title="Temas",
+        yaxis_title="Probabilidad",
+        font_color="#ffffff"
+    )
+    return fig
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
